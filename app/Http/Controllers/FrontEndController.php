d59ade1c7c09b7c59705f7147ffac4d39e5c97df
SuheL-Roy <60100467+SuheL-Roy@users.noreply.github.com>
2023-01-13 13:27:50 +0600

    updated

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Coupon;
+use App\Dish;
+use Cart;
+use Illuminate\Http\Request;
+
+class FrontEndController extends Controller
+{
+    public function index()
+    {
+        $dishes = Dish::where('dish_status', 1)->get();
+
+        return view('FrontEnd.include.home', compact('dishes') );
+    }
+    public function dish_show($category_id)
+    {
+        $categoryDish   = Dish::where('category_id', $category_id)
+                                ->where('dish_status', 1)
+                                ->get();
+                           
+      
+        return view('FrontEnd.include.dish', compact('categoryDish'));
+    }
+
+}
